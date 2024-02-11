b7de838f4ebd189fb80e7ce9e5a703f3520301dc
Juan Villa <ing.villa.jimenez@gmail.com>
2024-02-11 15:59:35 -0600

    Setting Up a New Project: Fast React Pizza Co.

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+/////////////////////////////////////////////////
+// Setting Up a New Project: Fast React Pizza Co.
+function App() {
+  const x = 23;
+
+  return <div>Hello Vite!</div>;
+}
+
+export default App;
