df3b2dc14c92574f8d751dcf359963461086df40
Juan Villa <ing.villa.jimenez@gmail.com>
2024-02-11 22:03:06 -0600

    A New Way Of Implementing Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+///////////////////////////////////
+// A New Way Of Implementing Routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
+
 /////////////////////////////////////////////////
 // Setting Up a New Project: Fast React Pizza Co.
 ///////////////////////////////////////////
 // Setting Up a Professional File Structure
 function App() {
-  const x = 23;
-
-  return <div>Hello Vite!</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
