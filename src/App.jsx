d91640a71d1f9a4c9925a50e70042fc5a9caea94
Juan Villa <ing.villa.jimenez@gmail.com>
2024-02-11 19:56:44 -0600

    Setting Up a Professional File Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 /////////////////////////////////////////////////
 // Setting Up a New Project: Fast React Pizza Co.
+///////////////////////////////////////////
+// Setting Up a Professional File Structure
 function App() {
   const x = 23;
 
