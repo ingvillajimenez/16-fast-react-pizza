31073cd39db8fad55c4b02d7d936528622660e3e
Juan Villa <ing.villa.jimenez@gmail.com>
2024-02-13 12:09:51 -0600

    Using Flexbox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,7 @@
 function Username() {
   ///////////////////////////////////////////////
   // The Box Model: Spacing, Borders, and Display
-  return <div className="hidden text-sm font-semibold">Jonas</div>;
+  return <div className="hidden text-sm font-semibold md:block">Jonas</div>;
 }
 
 export default Username;
