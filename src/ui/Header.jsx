bf95277e7179f51abddd962e2e0447c0aba9b780
Juan Villa <ing.villa.jimenez@gmail.com>
2024-02-12 22:30:16 -0600

    Working With Color

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,8 +2,10 @@ import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
+  /////////////////////
+  // Working With Color
   return (
-    <header>
+    <header className="bg-yellow-500">
       <Link to="/">Fast React Pizza Co.</Link>
 
       <SearchOrder />
