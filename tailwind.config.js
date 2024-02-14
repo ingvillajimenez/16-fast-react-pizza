6f2db0a1935441edb2408f1a636d42efe287db15
Juan Villa <ing.villa.jimenez@gmail.com>
2024-02-14 10:30:11 -0600

    Configuring Tailwind: Custom Font Family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+///////////////////////////////////////////
+// Configuring Tailwind: Custom Font Family
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+
+    extend: {
+      fontSize: {
+        huge: ["80rem", { lineHeight: "1" }],
+      },
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
